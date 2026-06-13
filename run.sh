e24deb6694ef7ba4f703928a9d63a7906f39ba5b
thnesss <Vane5sak@yandex.ru>
2026-06-13 14:21:36 +0000

    Создала Dockerfile для аналитика данных, написала в bash комманды для аналитика данных и написала комманды из основного скрипта

--- run.sh
+++ run.sh
@@ -12,4 +12,22 @@ case "$1" in
   create_local_data)
     python3 gen/generate.py "$ldata"
     ;;
+  build_reporter)
+    docker build -t data-reporter ./rep
+    ;;
+  run_reporter)
+    docker run --rm -v "$data":/data data-reporter
+    ;;
+  structure)
+    find .
+    ;;
+  clear_data)
+    rm -f "$data"/*.csv "$data"/*.html
+    ;;
+  inside_generator)
+    docker run --rm -v "$data":/data --entrypoint sh data-generator -c "ls -la /data"
+    ;;
+  inside_reporter)
+    docker run --rm -v "$data":/data --entrypoint sh data-reporter -c "ls -la /data"
+    ;;
 esac
